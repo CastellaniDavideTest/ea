caaeaac0f7c06f1e230820520cb7989f1a51b78b
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2021-01-03 16:35:44 +0100

    Created bin/ea.py

---
+++ bin/ea.py
@@ -0,0 +1,19 @@
+"""ea
+"""
+
+__author__ = "help@castellanidavide.it"
+__version__ = "1.0 2021-1-3"
+
+class ea:
+	def __init__ (self):
+		"""Where it all begins
+		"""
+		print(ea.ea())
+	
+	def ea():
+		"""ea first funtion
+		"""
+		return "ea" #for the test (see test_ea.py)
+		
+if __name__ == "__main__":
+	ea()
